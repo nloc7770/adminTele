372f645c4277875d990ae111b434ad5687981bb9
locnd2905 <locnd@galaxy.com.vn>
2023-05-10 23:52:12 +0700

    trigger build

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,6 @@
 import AuthUI from "./Auth";
 export default function Register() {
+    
     return (
         <div style={{ width: "100wh", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <div style={{ width: "30%", height: "50%" }}>
